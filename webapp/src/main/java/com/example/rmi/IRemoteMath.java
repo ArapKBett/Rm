75fc76d7e644f7d6985a2de497dd2f0ca995df1e
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-13 14:17:47 +0300

    Create IRemoteMath.java

---
+++ webapp/src/main/java/com/example/rmi/IRemoteMath.java
@@ -0,0 +1,9 @@
+package com.example.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IRemoteMath extends Remote {
+    double add(double i, double j) throws RemoteException;
+    double subtract(double i, double j) throws RemoteException;
+}
